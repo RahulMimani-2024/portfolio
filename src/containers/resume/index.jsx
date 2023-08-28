ed2fc66e2d140fb99bf78b49fd20de292140622b
Rahul Mimani <rahul.mimani.cd.mec20@itbhu.ac.in>
2023-08-28 16:19:30 +0530

    download resume

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent/index";
-import { FcBusinessman } from "react-icons/fc";
+import { BsDownload } from "react-icons/bs";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -8,13 +8,25 @@ import {
 import { data } from "./urils";
 import { MdWork } from "react-icons/md";
 import "./styles.scss";
+import pdf from "../../utils/rahul_mimani_sde.pdf"
 import "react-vertical-timeline-component/style.min.css";
 const Index = () => {
+  const handleDownload = () => {
+    // Replace 'your-file-url' with the actual URL of the file you want to download.
+    console.log("insde");
+    const fileUrl = pdf;
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.setAttribute('download', 'resume.pdf'); // Replace 'filename.ext' with the desired file name and extension.
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <section id="resume" className="resume">
       <PageHeaderContent
         headerText="my resume"
-        icon={<FcBusinessman size={40} />}
+        icon={<BsDownload size={40} style={{"cursor" : "pointer"}} onClick={handleDownload}/>}
       ></PageHeaderContent>
       <div className="timeline">
         <div className="timeline__experience">
